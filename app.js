d3c41c18cd40c1598758fb5a44794f98bed6b86a
Jacob Robinett <robinett258@gmail.com>
2022-03-06 19:50:00 -0600

    edit

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var connection = mysql.createConnection({
   host: "208.76.87.109",
   user: "aogmapax_user",
   password: "secret"
-  //database: "aogmapax_test"
-
   // host: "localhost",
   // user: "root",
   // password: "secret"
@@ -19,11 +17,22 @@ connection.connect((err) => {
     console.log('Connected to MySQL Server!');
 });
 
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM products", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+
 app.get('/', (req, res) => {
   //connection.query('SELECT * FROM activities')
   res.send("Hello");
   let result = connection.query('SELECT * FROM battery');
   res.send(result);
+  console.log("hello")
   // const temp = {a: "Apple"}
   // res.status(200).json(temp);
 })
